24489fd23d505a0b328aba5f7ad79050536f8ab8
Spicy Moo Deng <188100478+jo-price@users.noreply.github.com>
2025-02-01 17:28:59 -0600

    imported inquirer and added first prompt

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 // TODO: Include packages needed for this application
+import inquirer from "inquirer";
+
+inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of the project.',
+    } ,
+    {
+        type: 'input',
+    }
+    ])
 
 // TODO: Create an array of questions for user input
 const questions = [];
