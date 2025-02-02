e3faef2e7824f63117b35dbfcd55fdb2ad3021b1
Spicy Moo Deng <188100478+jo-price@users.noreply.github.com>
2025-02-02 12:10:36 -0600

    add all prompts and array

--- index.js
+++ index.js
@@ -10,11 +10,33 @@ inquirer
     } ,
     {
         type: 'input',
+        name: 'description',
+        message: 'Enter the project description.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information.',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions.',
     }
     ])
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['title', 'description', 'description', 'installation', 'usage', 'contributing', 'tests'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
