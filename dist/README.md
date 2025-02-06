34a434ab42dab20d2f46af44b6a270a18ff2e14a
Spicy Moo Deng <188100478+jo-price@users.noreply.github.com>
2025-02-05 21:34:56 -0600

    done

---
+++ dist/README.md
@@ -0,0 +1,24 @@
+
+# awgawegwaeg
+
+## Description
+awgawge
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [License](#license)
+* [Questions](#questions)
+## Installation
+awegawega
+## Usage
+awgawegaw
+## Contributors
+agwaegwa
+## Testing
+3agwwagaw
+## License
+Licensed under the MPL2.0 License.
+## Questions
+For any questions, reach out via [email](mailto:awgaega) or visit on [GitHub](https://github.com/aaeg).
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,8 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-// function LicenseBadge(license) {
-    // if (license !== "none") {
-      // return `![Github license](https://img.shields.io/badge/license-${data.license}-blue.svg)`;
-    // }
-    // return "";
-  // } ${LicenseBadge(data.license)}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `#${data.title}
+
+  return `
+# ${data.title}
 
 ## Description
 ${data.description}
@@ -31,17 +24,7 @@ ${data.tests}
 ## License
 Licensed under the ${data.license} License.
 ## Questions
-For any questions, reach out via [email](mailto:${data.email}) or visit ${data.userame} on [GitHub][def].
-[def]: https://github.com/${data.username}
+For any questions, reach out via [email](mailto:${data.email}) or visit on [GitHub](https://github.com/${data.username}).
 `
 }
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-// function LicenseSection(license) {
-  // if (license !== "none") {
-    // return `Licensed under the ${data.license} License.`;
-  // }
-  // return "";}
-
 export default generateMarkdown;
